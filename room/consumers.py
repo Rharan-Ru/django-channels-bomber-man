826057b414f1d0b0e7b3b5911226d4c3292b7673
Monke001 <rharanru@gmail.com>
2022-01-24 00:54:35 -0300

    First Release

--- room/consumers.py
+++ room/consumers.py
@@ -1,36 +1,72 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+players = {}
+
 
 class RoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        pass
-        # self.room_group_name = 'Game-Room'
-        #
-        # await self.channel_layer.group_add(
-        #     self.room_group_name,
-        #     self.channel_name,
-        # )
-        # await self.accept()
+        self.room_group_name = 'Game-Room'
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name,
+        )
+
+        await self.accept()
 
     async def disconnect(self, code):
-        pass
-        # await self.channel_layer.group_discard(
-        #     self.room_group_name,
-        #     self.channel_name,
-        # )
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name,
+        )
 
     async def receive(self, text_data=None, bytes_data=None):
-        pass
-        # receive_dict = json.loads(text_data)
-        #
-        # await self.channel_layer.group_send(
-        #     self.room_group_name,
-        #     {
-        #         'type': 'control_room',
-        #         'receive_dict': receive_dict,
-        #     }
-        # )
+        receive_dict = json.loads(text_data)
+        print(receive_dict)
+        if 'new-player' in receive_dict:
+            new_player = receive_dict['new-player']
+            if new_player not in players:
+                players[new_player] = {'pos_x': 0, 'pos_y': 0}
+            print(players)
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'control_room',
+                    'new-player': new_player,
+                    'other-players': players,
+                }
+            )
+        elif 'pos-x' in receive_dict or 'pos-y' in receive_dict:
+            player = receive_dict['player']
+            pos_x = receive_dict['pos-x']
+            pos_y = receive_dict['pos-y']
+            players[player] = {'pos_x': pos_x, 'pos_y': pos_y}
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'move_players',
+                    'player': player,
+                    'pos-x': pos_x,
+                    'pos-y': pos_y,
+                }
+            )
 
     async def control_room(self, event):
-        pass
+        new_player = event['new-player']
+        other_players = event['other-players']
+        print(event)
+        await self.send(json.dumps({
+            'playerData': new_player,
+            'otherPlayers': other_players,
+        }))
+
+    async def move_players(self, event):
+        player = event['player']
+        pos_x = event['pos-x']
+        pos_y = event['pos-y']
+        await self.send(json.dumps({
+            'player': player,
+            'posX': pos_x,
+            'posY': pos_y,
+        }))
--- static/js/room.js
+++ static/js/room.js
@@ -1,21 +1,91 @@
-document.onkeydown = KeyPressed;
+console.log('Hello guys');
+
+var usernameInput = document.querySelector('#username');
+var btnJoin = document.querySelector('#btn-join');
+var username;
+var webSocket;
+
+
+btnJoin.addEventListener('click', () => {
+    username = usernameInput.value;
+    console.log('username: ', username);
+    if (username == ''){
+        return;
+    };
+
+    usernameInput.value = '';
+    usernameInput.disabled = true;
+    usernameInput.style.visibility = 'hidden';
+
+    btnJoin.disabled = true;
+    btnJoin.style.visibility = 'hidden';
+
+    var labelUsername = document.querySelector('#label-username');
+    labelUsername.innerHTML = username;
+
+    var loc = window.location;
+    var wsStart = window.location.protocol == "https:" ? "wss://" : "ws://";
+
+    var endPoint = wsStart + loc.host + loc.pathname;
+    console.log(endPoint);
+
+    webSocket = new WebSocket(endPoint);
+    webSocket.addEventListener('open', (e) => {
+        console.log('Connection Open');
+        newPlayer(username);
+    });
+    webSocket.addEventListener('close', (e) => {
+        console.log('Connection Closed')
+    });
+    webSocket.addEventListener('error', (e) => {
+        console.log('Error', e)
+    });
+
+    webSocket.onmessage = function(e) {
+        var data = JSON.parse(e.data);
+        console.log(data);
+        if (data.playerData) {
+//            createPlayer(data.playerData);
+            createOtherPlayers(data.otherPlayers);
+        };
+        if (data.player) {
+            changePosition(data.player, data.posX, data.posY);
+        };
+    };
+});
+
+function newPlayer(player) {
+    webSocket.send(JSON.stringify({
+        'new-player': player,
+    }));
+};
+
+//function createPlayer(user) {
+//    console.log(user);
+//    var arena = document.querySelector('#background');
+//    var Player = document.createElement('div');
+//    Player.id = user;
+//    Player.className = 'players';
+//    arena.appendChild(Player);
+//};
+
 var BeginningPosX = 0;
 var BeginningPosY = 0;
 var edgeX = 360;
 var edgeY = 360;
-
+document.onkeydown = KeyPressed;
 function KeyPressed(k) {
+    Player = document.querySelector('#' + username);
 
     var LeftBtn = 37;
     var RightBtn = 39;
     var UpBtn = 38;
     var DownBtn = 40;
 
-    var Player = document.getElementById("monke");
     console.log(k.keyCode);
     if (k.keyCode == RightBtn) {
         console.log(BeginningPosX);
-        BeginningPos = BeginningPosX += 40;
+        BeginningPosX = BeginningPosX += 40;
         if (BeginningPosX > edgeX) {
             BeginningPosX = edgeX;
         };
@@ -23,7 +93,7 @@ function KeyPressed(k) {
     }
     else if (k.keyCode == LeftBtn) {
         console.log(BeginningPosX);
-        BeginningPos = BeginningPosX -= 40;
+        BeginningPosX = BeginningPosX -= 40;
         if (BeginningPosX < 0) {
             BeginningPosX = 0;
         };
@@ -31,7 +101,7 @@ function KeyPressed(k) {
     }
     else if (k.keyCode == UpBtn) {
         console.log(BeginningPosY);
-        BeginningPos = BeginningPosY -= 40;
+        BeginningPosY = BeginningPosY -= 40;
         if (BeginningPosY < 0) {
             BeginningPosY = 0;
         };
@@ -39,10 +109,36 @@ function KeyPressed(k) {
     }
     else if (k.keyCode == DownBtn) {
         console.log(BeginningPosY);
-        BeginningPos = BeginningPosY += 40;
+        BeginningPosY = BeginningPosY += 40;
         if (BeginningPosY > edgeY) {
             BeginningPosY = edgeY;
         };
         Player.style.top = BeginningPosY + "px";
-    }
+    };
+    webSocket.send(JSON.stringify({
+        'player': Player.id,
+        'pos-x': BeginningPosX,
+        'pos-y': BeginningPosY,
+    }));
+};
+
+function changePosition(player, x, y) {
+    Player = document.querySelector('#' + player);
+    Player.style.left = x + "px";
+    Player.style.top = y + "px";
 }
+
+function createOtherPlayers(users) {
+    var arena = document.querySelector('#background');
+    arena.innerHTML = '';
+    console.log(users)
+    for (player in users) {
+        console.log(player, users[player]);
+        var Player = document.createElement('div');
+        Player.id = player;
+        Player.className = 'players';
+        Player.style.left = users[player].pos_x + "px";
+        Player.style.top = users[player].pos_y + "px";
+        arena.appendChild(Player);
+    };
+};
