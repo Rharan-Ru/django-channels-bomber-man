4546fd6f2395876021a9377f11ae6f618b1a6699
Monke001 <rharanru@gmail.com>
2022-01-28 03:27:41 -0300

    Improve room-data, and remove player function

--- room/consumers.py
+++ room/consumers.py
@@ -7,7 +7,8 @@ players = {}
 class RoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_group_name = 'Game-Room'
-
+        print('Connected')
+        print(self.channel_name)
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name,
@@ -16,6 +17,7 @@ class RoomConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, code):
+        print('Disconnected')
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name,
@@ -23,18 +25,31 @@ class RoomConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data=None, bytes_data=None):
         receive_dict = json.loads(text_data)
-        print(receive_dict)
         if 'new-player' in receive_dict:
+            # Send playes data to new-user only
             new_player = receive_dict['new-player']
-            if new_player not in players:
-                players[new_player] = {'pos_x': 0, 'pos_y': 0}
-            print(players)
+            self.user_room = new_player
+            await self.channel_layer.group_add(
+                self.user_room,
+                self.channel_name,
+            )
+            if len(players) > 0:
+                await self.channel_layer.group_send(
+                    self.user_room,
+                    {
+                        'type': 'new_player',
+                        'other-players': players,
+                    }
+                )
+
+            # Send new-user data to all users connected
+            players[new_player] = {'pos_x': 0, 'pos_y': 0}
+            print("New Player")
             await self.channel_layer.group_send(
                 self.room_group_name,
                 {
                     'type': 'control_room',
                     'new-player': new_player,
-                    'other-players': players,
                 }
             )
         elif 'pos-x' in receive_dict or 'pos-y' in receive_dict:
@@ -51,17 +66,49 @@ class RoomConsumer(AsyncWebsocketConsumer):
                     'pos-y': pos_y,
                 }
             )
+        elif 'remove-player' in receive_dict:
+            player_removed = receive_dict['remove-player']
+            players.pop(player_removed)
+            print(f"Removed player: {player_removed}")
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'remove_player',
+                    'player-removed': player_removed,
+                }
+            )
 
     async def control_room(self, event):
-        new_player = event['new-player']
-        other_players = event['other-players']
+        print("Control-room data:")
         print(event)
+        new_player = event['new-player']
         await self.send(json.dumps({
             'playerData': new_player,
+        }))
+
+    async def new_player(self, event):
+        print("New-player data:")
+        print(event)
+        other_players = event['other-players']
+        await self.send(json.dumps({
             'otherPlayers': other_players,
+        }))
+        await self.channel_layer.group_discard(
+            self.user_room,
+            self.channel_name,
+        )
+
+    async def remove_player(self, event):
+        print("Remove-player data:")
+        print(event)
+        player_removed = event['player-removed']
+        await self.send(json.dumps({
+            'playerRemoved': player_removed,
         }))
 
     async def move_players(self, event):
+        print("Move player data:")
+        print(event)
         player = event['player']
         pos_x = event['pos-x']
         pos_y = event['pos-y']
--- static/js/room.js
+++ static/js/room.js
@@ -45,8 +45,13 @@ btnJoin.addEventListener('click', () => {
         var data = JSON.parse(e.data);
         console.log(data);
         if (data.playerData) {
-//            createPlayer(data.playerData);
+            createPlayer(data.playerData);
+        };
+        if (data.otherPlayers) {
             createOtherPlayers(data.otherPlayers);
+        };
+        if (data.playerRemoved) {
+            removePlayer(data.playerRemoved);
         };
         if (data.player) {
             changePosition(data.player, data.posX, data.posY);
@@ -60,80 +65,98 @@ function newPlayer(player) {
     }));
 };
 
-//function createPlayer(user) {
-//    console.log(user);
-//    var arena = document.querySelector('#background');
-//    var Player = document.createElement('div');
-//    Player.id = user;
-//    Player.className = 'players';
-//    arena.appendChild(Player);
-//};
-
 var BeginningPosX = 0;
 var BeginningPosY = 0;
-var edgeX = 360;
+var edgeX = 560;
 var edgeY = 360;
-document.onkeydown = KeyPressed;
-function KeyPressed(k) {
-    Player = document.querySelector('#' + username);
+function initGame(){
+    document.onkeydown = KeyPressed;
+    function KeyPressed(k) {
+        Player = document.querySelector('#' + username);
+        var LeftBtn = 37;
+        var RightBtn = 39;
+        var UpBtn = 38;
+        var DownBtn = 40;
+        var bomb = 90;
 
-    var LeftBtn = 37;
-    var RightBtn = 39;
-    var UpBtn = 38;
-    var DownBtn = 40;
-
-    console.log(k.keyCode);
-    if (k.keyCode == RightBtn) {
-        console.log(BeginningPosX);
-        BeginningPosX = BeginningPosX += 40;
-        if (BeginningPosX > edgeX) {
-            BeginningPosX = edgeX;
+        console.log(k.keyCode);
+        switch (k.keyCode) {
+            case RightBtn:
+                console.log(BeginningPosX);
+                BeginningPosX = BeginningPosX += 40;
+                if (BeginningPosX > edgeX) {
+                    BeginningPosX = edgeX;
+                };
+                Player.style.left = BeginningPosX + "px";
+                webSocket.send(JSON.stringify({
+                    'player': Player.id,
+                    'pos-x': BeginningPosX,
+                    'pos-y': BeginningPosY,
+                }));
+                break;
+            case LeftBtn:
+                console.log(BeginningPosX);
+                BeginningPosX = BeginningPosX -= 40;
+                if (BeginningPosX < 0) {
+                    BeginningPosX = 0;
+                };
+                Player.style.left = BeginningPosX + "px";
+                webSocket.send(JSON.stringify({
+                    'player': Player.id,
+                    'pos-x': BeginningPosX,
+                    'pos-y': BeginningPosY,
+                }));
+                break;
+            case UpBtn:
+                console.log(BeginningPosY);
+                BeginningPosY = BeginningPosY -= 40;
+                if (BeginningPosY < 0) {
+                    BeginningPosY = 0;
+                };
+                Player.style.top = BeginningPosY + "px";
+                webSocket.send(JSON.stringify({
+                    'player': Player.id,
+                    'pos-x': BeginningPosX,
+                    'pos-y': BeginningPosY,
+                }));
+                break;
+            case DownBtn:
+                console.log(BeginningPosY);
+                BeginningPosY = BeginningPosY += 40;
+                if (BeginningPosY > edgeY) {
+                    BeginningPosY = edgeY;
+                };
+                Player.style.top = BeginningPosY + "px";
+                webSocket.send(JSON.stringify({
+                    'player': Player.id,
+                    'pos-x': BeginningPosX,
+                    'pos-y': BeginningPosY,
+                }));
+                break;
+            case bomb:
+                console.log('Boom')
         };
-        Player.style.left = BeginningPosX + "px";
-    }
-    else if (k.keyCode == LeftBtn) {
-        console.log(BeginningPosX);
-        BeginningPosX = BeginningPosX -= 40;
-        if (BeginningPosX < 0) {
-            BeginningPosX = 0;
-        };
-        Player.style.left = BeginningPosX + "px";
-    }
-    else if (k.keyCode == UpBtn) {
-        console.log(BeginningPosY);
-        BeginningPosY = BeginningPosY -= 40;
-        if (BeginningPosY < 0) {
-            BeginningPosY = 0;
-        };
-        Player.style.top = BeginningPosY + "px";
-    }
-    else if (k.keyCode == DownBtn) {
-        console.log(BeginningPosY);
-        BeginningPosY = BeginningPosY += 40;
-        if (BeginningPosY > edgeY) {
-            BeginningPosY = edgeY;
-        };
-        Player.style.top = BeginningPosY + "px";
     };
-    webSocket.send(JSON.stringify({
-        'player': Player.id,
-        'pos-x': BeginningPosX,
-        'pos-y': BeginningPosY,
-    }));
 };
 
 function changePosition(player, x, y) {
     Player = document.querySelector('#' + player);
     Player.style.left = x + "px";
     Player.style.top = y + "px";
-}
+};
+
+function createPlayer(user) {
+    var arena = document.querySelector('#background');
+    var Player = document.createElement('div');
+    Player.id = user;
+    Player.className = 'players';
+    arena.appendChild(Player);
+    initGame();
+};
 
 function createOtherPlayers(users) {
     var arena = document.querySelector('#background');
-    arena.innerHTML = '';
-    console.log(users)
     for (player in users) {
-        console.log(player, users[player]);
         var Player = document.createElement('div');
         Player.id = player;
         Player.className = 'players';
@@ -141,4 +164,20 @@ function createOtherPlayers(users) {
         Player.style.top = users[player].pos_y + "px";
         arena.appendChild(Player);
     };
+};
+
+function removePlayer(user) {
+    var player = document.querySelector('#' + user);
+    player.parentNode.removeChild(player);
+};
+
+window.onbeforeunload = function() {
+    // Send exit message to websocket
+    webSocket.send(JSON.stringify({
+        'remove-player': username,
+    }));
+    window.setTimeout(function () {
+        window.location = '/';
+    }, 0);
+    window.onbeforeunload = null; // necessary to prevent infinite loop, that kills your browser
 };
